60f35a13a3dfbde879667bb65b8ffd3c06aca342
Bruno Maciel <brunom4ciel@gmail.com>
2018-06-06 12:07:57 -0300

    Update java-install.sh

--- setup/dependencies/java-install.sh
+++ setup/dependencies/java-install.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository ppa:webupd8team/java -y
 
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
-sudo apt-get install oracle-java8-set-default -y
+#sudo apt-get install oracle-java8-set-default -y
 
 #sudo apt-get install oracle-java9-installer -y
 #sudo apt-get install oracle-java9-set-default -y
